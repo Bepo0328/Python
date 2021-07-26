58da96f157fe1ae42f196591d9636197df3f2d1a
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-07-26 23:33:05 +0900

    문제 풀기 Exercise 30번까지

---
+++ 파이썬 반복문과 리스트.py
@@ -0,0 +1,71 @@
+# Exercise 31. 반복문
+# 1 ~ 10까지의 숫자에 대해 모두 더한 값을 출력하는 프로그램을 for 문을 사용하여 작성하세요.
+def Excercis_31():
+    print()
+
+# Exercise 32. 반복문
+# 사용자로부터 2 ~ 9 사이의 숫자를 입력 받은 후, 해당 숫자에 대한 구구단을 출력하세요.
+def Excercis_32():
+    print()
+
+# Exercise 33. 반복문과 문자열 다루기 (split)
+# 사용자로부터 , 로 구분된 여러 이름을 입력받아서, 한 줄에 하나씩 이름을 출력하세요
+# 사용자 입력: Dave,David,Andy,Arthor
+# 출력 예:
+# Dave
+# David
+# Andy
+# Arthor
+def Excercis_33():
+    print()
+
+# Exercise 34. 반복문과 문자열 다루기 (split)
+# 사용자로부터 [이름1],[이름2],[이름3] 과 같은 형식으로 데이터를 입력받아서, 한 줄에 하나씩 이름을 출력하세요
+# 사용자 입력: [Dave],[David],[Andy],[Arthor]
+# 출력 예:
+# Dave
+# David
+# Andy
+# Arthor
+def Excercis_34():
+    print()
+
+# Exercise 35. 반복문과 조건문
+# 1부터 30까지의 숫자 중 3의 배수만 출력하세요.
+def Excercis_35():
+    print()
+
+# Exercise 36. 반복문 (while)
+# 1부터 100까지 숫자를 모두 더한 값을 출력하세요.
+# 단 while 구문을 사용해서 작성하세요.
+def Excercis_36():
+    print()
+
+# Exercise 37. 반복문 (while)
+# 사용자로부터 4자리의 숫자로 구성된 데이터를 입력받아서
+# 비밀번호와 같으면 '비밀번호가 맞습니다.'를 출력하고 종료하세요.
+# 비밀번호와 다르면 '비밀번호가 틀렸습니다.'를 출력하고 다시 사용자로부터 데이터를 입력받으세요.
+# 비밀번호는 4312 입니다.
+def Excercis_37():
+    print()
+
+# Exercise 38. 데이터 구조와 반복문 (리스트)
+# 다음 리스트 변수에서 음수 데이터를 삭제하고, 양수만 가진 리스트 변수로 만들고, 해당 변수를 출력하세요.
+# num_list = [0, -11, 31, 22, -11, 33, -44, -55]
+def Excercis_38():
+    print()
+
+# Exercise 39. 데이터 구조와 반복문 (리스트)
+# 다음 리스트에 있는 데이터의 길이를 한 라인에 하나씩 출력하세요.
+# list_data = ["fun-coding", "Dave", "Linux", "Python", "javascript", "front-end", "back-end", "dataengineering"]
+def Excercis_39():
+    print()
+
+# Exercise 40. 데이터 구조와 반복문 (리스트)
+# 다음 리스트에 있는 숫자를 역 방향으로 출력하세요.
+# 단, 리스트에 있는 숫자들은 한 라인에 하나씩 출력하세요.
+def Excercis_40():
+    print()
+
+if __name__ == '__main__':
+    Excercis_31()
--- 파이썬 조건문과 문자열.py
+++ 파이썬 조건문과 문자열.py
@@ -94,8 +94,106 @@ def Excercis_20():
 
 # Exercise 21. 문자열 다루기 (strip)
 # 다음 문자열에서 ...를 제거하라.
+# mystr = "a man goes into the room..."
 def Excercis_21():
-    print()
+    mystr = "a man goes into the room..."
+    print(mystr.strip("."))
+
+# Exercise 22. 문자열 다루기 (strip)
+# 주식 종목을 나타내는 종목코드에 공백과 줄바꿈 기호가 포함되어 있다. 공백과 잘바꿈 기호를 제거하고 종목코드만을 추출하라.
+# code = '         000660\n            '
+# 출력: '000660'
+def Excercis_22():
+    code = '         000660\n            '
+    print(code.strip(' \n'))
+
+# Exercise 23. 문자열 다루기 (count)
+# 다음 문자열에서 'Python' 문자열의 빈도수를 출력하라.
+# python_desc = "Python is an interpreted high-level programming language for general-purpose programming. Created by Guido van Rossum and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant whitespace."
+# 출력 예: 2
+def Excercis_23():
+    python_desc = "Python is an interpreted high-level programming language for general-purpose programming. Created by Guido van Rossum and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant whitespace."
+    count = python_desc.count("Python")
+    print(count)
+
+# Exercise 24. 문자열 다루기 (count)
+# 다음 문자열에서 'p' 문자가 몇번 나오는지 빈도수를 출력하라.
+# python_desc = "Python is an interpreted high-level programming language for general-purpose programming. Created by Guido van Rossum and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant whitespace."
+# 출력 예: 9
+def Excercis_24():
+    python_desc = "Python is an interpreted high-level programming language for general-purpose programming. Created by Guido van Rossum and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant whitespace."
+    count = python_desc.count("p")
+    print(count)
+
+# Exercise 25. 문자열 다루기 (문자열 인덱싱)
+# letters 라는 변수에 들어 있는 문자열에서 두 번째와 네 번째 문자를 출력하라
+# letters = "python"
+# 출력 예:
+# y
+# h
+def Excercis_25():
+    letters = "python"
+    print(letters[1])
+    print(letters[3])
+
+# Exercise 26. 문자열 다루기 (문자열 인덱싱)
+# letters 라는 변수에 사용자로부터 문자열을 입력받아서 문자 n 이 들어있는지를 출력하라 ( n 이 들어 있으면 0, 안들어있으면 -1을 출력하라)
+def Excercis_26():
+    letters = input("입력해주세요: ")
+    if letters.find("n") >= 0:
+        print ("0")
+    else:
+        print ("-1")
+
+# Exercise 27. 문자열 다루기 (문자열 인덱싱)
+# letters 라는 변수에 사용자로부터 문자열을 입력받아서 문자 n 이 들어있는지를 출력하라
+# n 이 들어 있으면 'n 이 들어있습니다.', 안들어있으면 'n 이 안들어있습니다.' 를 출력하라
+def Excercis_27():
+    letters = input("입력해주세요: ")
+    if letters.find("n") >= 0:
+        print("n 이 들어있습니다.")
+    else:
+        print("n 이 안들어있습니다.")
+
+# Exercise 28. 문자열 다루기 (문자열 인덱싱)와 조건문
+# 주민등록번호의 뒷 자리 7자리 중 두번째부터 세번째는 출생 지역 코드입니다.
+# 다음 표를 참조하여 사용자로부터 주민 등록 번호를 입력 받은 후 출생지를 출력하세요.
+# 지역 코드	출생 지역
+# 00 ~ 08	서울
+# 09 ~ 12	부산
+def Excercis_28():
+    inputStr = input("주민등록번호를 입력해주세요: ")
+    inputStr = int(inputStr[8:10])
+    print(inputStr)
+    if inputStr >= 0 and inputStr <= 8:
+        print ("서울")
+    elif inputStr >= 9 and inputStr <= 12:
+        print ("부산")
+    else:
+        print("알수없습니다.")
+
+
+# Exercise 29. 문자열 다루기 (split)
+# letters 라는 변수에 Dave,David,Andy,2222,3123123,LLL 가 들어있다. 해당 변수값을 , 를 기준으로 분리해서 출력하라
+# 출력 예: 
+# Dave
+# David
+# Andy
+# 2222
+# 3123123
+# LLL
+def Excercis_29():
+    letters = "Dave,David,Andy,2222,3123123,LLL"
+    lettersList = letters.split(",")
+    for letter in lettersList:
+        print(letter)
+
+# Exercise 30. 문자열 다루기 (split)
+# 다음과 같은 파일 이름(확장자 포함)에서 확장자를 제거한 파일 이름만 출력하세요.
+# filename = 'exercise01.docx'
+def Excercis_30():
+    filename = 'exercise01.docx'
+    print(filename.split(".")[0])
 
 if __name__ == '__main__':
-    Excercis_20()
+    Excercis_30()
